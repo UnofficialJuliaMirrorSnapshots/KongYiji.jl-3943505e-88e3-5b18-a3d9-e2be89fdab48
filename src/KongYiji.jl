0af2486ed328150d8a9b99c6eee7ce405ce651db
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-09-08 07:46:57 +0000

    Snapshot of branch master taken on 2019-09-08T03:46:54.353-04:00 from "https://github.com/UnofficialJuliaMirror/KongYiji.jl-3943505e-88e3-5b18-a3d9-e2be89fdab48"

--- src/KongYiji.jl
+++ src/KongYiji.jl
@@ -1,20 +1,35 @@
 module KongYiji
 
-import Base: size, split, display, read, write, first, stat, push!, length, iterate, ==, getindex, summary, collect, values, isless, get, eachmatch
+import Base: size, split, display, read, write, first, stat, push!, length, iterate, ==, getindex, summary, collect, values, isless, get, eachmatch, in
 
 using Pkg
 using JLD2, FileIO
-using Random, DataStructures, ProgressMeter, DelimitedFiles
+using Random, DataStructures, ProgressMeter, DelimitedFiles, DataFrames
 
-export Kong, postable, h2vtable, v2htable, h2htable, hprtable
+export postable, h2vtable, v2htable, h2htable, hprtable, kwictable,
+        ChTreebank, train!, HMM, ooHMM, LM, ZhuXian, posidsents, wordsents, rawsents,
+        UselessTable, generate!
 
-include("UselessTable.jl")
-include("zip7.jl")
-include("AhoCorasickAutomaton.jl")
+
+const Ti = UInt32
+const Tv = Float32
+const oo = Tv(Inf)
+
+include("utils.jl")
+
 include("CtbTree.jl")
 include("ChTreebank.jl")
+
+include("ZhuXian.jl")
+
+include("UselessTable.jl")
+include("compress.jl")
+include("AhoCorasickAutomaton.jl")
 include("HMM.jl")
 include("HmmScoreTable.jl")
 include("HmmDebug.jl")
+include("IcwbScoreTable.jl")
+include("LM.jl")
+include("ooHMM.jl")
 
 end # module
